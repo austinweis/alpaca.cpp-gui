5e45af5f60ad6e69ab1906393f2192fd945ca989
Austin Weis <austinweis@protonmail.com>
2023-09-05 19:11:42 -0600

    Automatic environment creation

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
-# supply path to gguf model as first argument
+#!/bin/sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+export MODEL_PATH=$1 # take first argument as relative path to gguf model
 
-export MODEL_PATH=$1
-source .venv/bin/activate
-flask --app chat-app run -h 0.0.0.0
+if [ ! -d "$SCRIPT_DIR/.venv" ]; then
+    echo "Creating virtual environment..."
+    python -m venv $SCRIPT_DIR/.venv
+fi
+source $SCRIPT_DIR/.venv/bin/activate
+
+echo "Updating packages..."
+python -m pip install -r $SCRIPT_DIR/requirements.txt
+
+echo "Starting server..."
+flask --app $SCRIPT_DIR/chat-app run -h 0.0.0.0
